9c85c6f88b00d0451c82927c469739cabb4ff06b
Serge Sukhanov <ssuhanov@icloud.com>
2017-07-16 17:29:00 +0300

    Add history view controller

---
+++ Phoneword_iOS/CallHistoryController.cs
@@ -0,0 +1,13 @@
+using Foundation;
+using System;
+using UIKit;
+
+namespace Phoneword_iOS
+{
+    public partial class CallHistoryController : UITableViewController
+    {
+        public CallHistoryController (IntPtr handle) : base (handle)
+        {
+        }
+    }
+}
