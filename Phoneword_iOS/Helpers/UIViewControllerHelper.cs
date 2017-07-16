4e129a74bfb1aeff8b3ea1dacbf86e48b9518bf9
Serge Sukhanov <ssuhanov@icloud.com>
2017-07-16 18:02:36 +0300

    Implement helper for separate storyboards

---
+++ Phoneword_iOS/Helpers/UIViewControllerHelper.cs
@@ -0,0 +1,13 @@
+﻿using UIKit;
+
+namespace Phoneword_iOS
+{
+    public static class UIViewControllerHelper
+    {
+        public static T StoryboardInstance<T>() where T : UIViewController
+        {
+            UIStoryboard storyboard = UIStoryboard.FromName(typeof(T).Name, null);
+            return storyboard.InstantiateInitialViewController() as T;
+        }
+    }
+}
